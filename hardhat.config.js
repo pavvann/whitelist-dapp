4172f8b453badc959acd0cd7cae936675e61cd55
Pawan <pawanbajaj2012@gmail.com>
2022-11-16 02:08:20 +0530

    added urls

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,10 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
   solidity: "0.8.17",
   defaultNetwork: "hardhat",
